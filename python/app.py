693d35c30d74396bdf68d1d6a845420113785573
czeflorian <florianczeczil@outlook.de>
2022-03-28 09:28:12 +0200

    dockerized python app

--- python/app.py
+++ python/app.py
@@ -1,6 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-	return "Hello Flask!"
+    return "Hello Flask!"
+
+
+if __name__ == "__main__":
+    from waitress import serve
+    print("Server now running on http://0.0.0.0:8080")
+    serve(app, host="0.0.0.0", port=8080)
