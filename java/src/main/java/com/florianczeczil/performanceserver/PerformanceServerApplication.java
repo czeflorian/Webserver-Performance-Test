9943be39f243609a167a95416fe4980a188a1850
czeflorian <florianczeczil@outlook.de>
2022-03-28 08:36:12 +0200

    init java

---
+++ java/src/main/java/com/florianczeczil/performanceserver/PerformanceServerApplication.java
@@ -0,0 +1,16 @@
+package com.florianczeczil.performanceserver;
+
+import java.util.Arrays;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class PerformanceServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PerformanceServerApplication.class, args);
+  }
+}
