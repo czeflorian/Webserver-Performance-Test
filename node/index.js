afb9b64bb86ff50b26093c688b2161dec77382fb
czeflorian <florianczeczil@outlook.de>
2022-03-28 13:12:01 +0200

    added content type to node app

--- node/index.js
+++ node/index.js
@@ -3,11 +3,11 @@ const app = express();
 const port = 8080;
 
 app.get("/ok", (req, res) => {
-  return res.status(200).send();
+  res.status(200).setHeader("Content-Type", "text/plain").send();
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello from Node!");
+  res.setHeader("Content-Type", "text/plain").send("Hello from Node!");
 });
 
 app.listen(port, () => {
