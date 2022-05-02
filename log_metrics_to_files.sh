69ecfc6d13486269f27d498c4d43e3c50c4327c7
czeflorian <florianczeczil@outlook.de>
2022-05-02 16:05:28 +0200

    changed shell script to automatically generate server metrics

---
+++ log_metrics_to_files.sh
@@ -0,0 +1,16 @@
+mkdir metrics/
+
+echo "Container,CPU Usage,Memory Usage;" > metrics/node_metrics.csv
+echo "Container,CPU Usage,Memory Usage;" > metrics/java_metrics.csv
+echo "Container,CPU Usage,Memory Usage;" > metrics/go_metrics.csv
+echo "Container,CPU Usage,Memory Usage;" > metrics/php_metrics.csv
+echo "Container,CPU Usage,Memory Usage;" > metrics/python_metrics.csv
+
+while true
+do
+	docker stats --no-stream --format "{{.Container}},{{.CPUPerc}},{{.MemUsage}};" go >> metrics/go_metrics.csv
+	docker stats --no-stream --format "{{.Container}},{{.CPUPerc}},{{.MemUsage}};" java >> metrics/java_metrics.csv
+	docker stats --no-stream --format "{{.Container}},{{.CPUPerc}},{{.MemUsage}};" node >> metrics/node_metrics.csv
+	docker stats --no-stream --format "{{.Container}},{{.CPUPerc}},{{.MemUsage}};" php >> metrics/php_metrics.csv
+	docker stats --no-stream --format "{{.Container}},{{.CPUPerc}},{{.MemUsage}};" python >> metrics/python_metrics.csv
+done
